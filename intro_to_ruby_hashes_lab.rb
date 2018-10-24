39232ff8f0006952943418e92a8ceaf348f79dae
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-24 13:30:49 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
       :three_pieces_owned=>100,
       :four_pieces_owned=>200},
     :names=>{
-      :reading_railroad=>{"mortgage_value"=>"$100"}
+      :reading_railroad=>{},
     }
   }
 }
